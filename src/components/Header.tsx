b4d9fbf3a12f2c8a97bec0c59f9cb5aa7969d34e
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 17:27:07 -0300

    feat(Creating and styling header component): :sparkles:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import logo from '../assets/img/logo.svg'
+import styles from './Header.module.css'
+
 export function Header() {
-  return <></>
+  return (
+    <header className={styles.header}>
+      <img className={styles.headerLogo} src={logo} alt="logo" />
+      <h1 className={styles.headerTitle}>
+        to<span>do</span>
+      </h1>
+    </header>
+  )
 }
