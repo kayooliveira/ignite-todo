e53f69217a33244da3e954597eded03ebe8a09ef
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 18:52:30 -0300

    feat(Importing TaskContainer to show it): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
 import { Header } from './components/Header'
+import { TaskContainer } from './components/TaskContainer'
 import { TaskForm } from './components/TaskForm'
 
 export function App() {
-  const [tasks, setTasks] = useState([''])
+  const [tasks, setTasks] = useState<string[]>([])
 
   function handleCreateNewTask(task: string) {
     setTasks([...tasks, task])
@@ -17,6 +18,7 @@ export function App() {
       <Header />
       <main>
         <TaskForm onSubmit={handleCreateNewTask} />
+        <TaskContainer tasks={tasks} />
       </main>
     </>
   )
