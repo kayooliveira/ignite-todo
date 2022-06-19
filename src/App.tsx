7afbe15686c587a0195cf7e44e4865dba0d79fea
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 18:19:58 -0300

    feat(Implementing tasks and function to create new one): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Header } from './components/Header'
+import { TaskForm } from './components/TaskForm'
 
 export function App() {
+  const [tasks, setTasks] = useState([''])
+
+  function handleCreateNewTask(task: string) {
+    setTasks([...tasks, task])
+  }
+
+  console.log(tasks)
+
   return (
     <>
       <Header />
+      <main>
+        <TaskForm onSubmit={handleCreateNewTask} />
+      </main>
     </>
   )
 }
