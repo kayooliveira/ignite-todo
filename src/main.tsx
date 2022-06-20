a75c6e296cbc5b45e455e796bb6303bae34682e8
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 21:38:57 -0300

    feat(implementing react-hot-toast): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './global.css'
+
+import { Toaster } from 'react-hot-toast'
 
 import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        style: {
+          background: '#5E60CE',
+          color: '#fff',
+          fontWeight: 'bold'
+        }
+      }}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
