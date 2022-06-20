6d84fd7863d308de33d463cc267c66fac42afbb2
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 21:39:26 -0300

    feat(Implementing functions to create, delete and set tasks as completed): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
+import { Prohibit } from 'phosphor-react'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { v4 } from 'uuid'
 
+import { TaskType } from './@types'
 import { Header } from './components/Header'
 import { TaskContainer } from './components/TaskContainer'
 import { TaskForm } from './components/TaskForm'
 
 export function App() {
-  const [tasks, setTasks] = useState<string[]>([])
+  const [tasks, setTasks] = useState<TaskType[]>([
+    {
+      id: v4(),
+      completed: false,
+      task: 'Comprar pão de queijo'
+    },
+    {
+      id: v4(),
+      completed: true,
+      task: 'Alterar dados dos usuários'
+    },
+    {
+      id: v4(),
+      completed: false,
+      task: 'Terminar a refatoração da tela de login'
+    }
+  ])
 
   function handleCreateNewTask(task: string) {
-    setTasks([...tasks, task])
+    const newId = v4()
+
+    const taskExistsAndIsNotComplete = tasks.find(
+      t => t.task === task && !t.completed
+    )
+
+    if (taskExistsAndIsNotComplete) {
+      return toast('Essa tarefa já existe!', {
+        icon: <Prohibit weight="bold" />
+      })
+    }
+    setTasks([...tasks, { id: newId, completed: false, task }])
   }
 
-  console.log(tasks)
+  function handleCompleteTask(id: string) {
+    const newTasks = tasks.map(task =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    )
+    setTasks(newTasks)
+  }
+
+  function handleDeleteTask(id: string) {
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
+  }
 
   return (
     <>
       <Header />
       <main>
         <TaskForm onSubmit={handleCreateNewTask} />
-        <TaskContainer tasks={tasks} />
+        <TaskContainer
+          onCompleteTask={handleCompleteTask}
+          onDeleteTask={handleDeleteTask}
+          tasks={tasks}
+        />
       </main>
     </>
   )
