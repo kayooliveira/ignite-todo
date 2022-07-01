e608b130aadc1519b559fb3a88bb39c006c5d36b
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-07-01 19:04:25 -0300

    feat(Changing animation directions): :art:

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -36,11 +36,11 @@ export function Task({ task, onDeleteTask, onCompleteTask }: TaskProps) {
   const motionVariants = {
     initial: {
       opacity: 0,
-      translateY: -40
+      translateX: -50
     },
     animate: {
       opacity: 1,
-      translateY: 0,
+      translateX: 0,
       transition: {
         duration: 0.2
       }
